2aec3294e1a8b96446f2ada29e27b58af723478d
Jo Carter <github@jocarter.co.uk>
2011-10-18 13:22:09 +0200

    Adding some useful bits to the default JS

--- data/files/web/js/main.js
+++ data/files/web/js/main.js
@@ -1,3 +1,11 @@
 window.addEvent('domready', function(){
   
+  
+  // Try and hide web debug to keep out of the way on dev
+  try { sfWebDebugToggleMenu(); } catch(e) { }
+  
+  // IE fix for errant console.log
+  if (typeof console === "undefined") {
+    console = { log: function() { } };
+  }
 });
