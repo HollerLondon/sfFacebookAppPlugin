5034e9eb872ac6b23ceca079e59aa1306329bd4b
Sergei K <sergey.krot@holler.co.uk>
2012-07-11 05:21:31 -0700

    adding checks for the environment so we do not enable logging or web_debug when its not needed.

--- config/sfFacebookAppPluginConfiguration.class.php
+++ config/sfFacebookAppPluginConfiguration.class.php
@@ -3,7 +3,14 @@ class sfFacebookAppPluginConfiguration extends sfPluginConfiguration
 {
   public function initialize()
   {
-    $this->dispatcher->connect('facebook.api_call',     array('sfWebDebugPanelFacebook', 'listenToApiCall'));
-    $this->dispatcher->connect('debug.web.load_panels', array('sfWebDebugPanelFacebook', 'listenToLoadDebugWebPanelEvent'));
+    if (sfConfig::get('sf_debug', false))
+    {
+      $this->dispatcher->connect('facebook.api_call',     array('sfWebDebugPanelFacebook', 'listenToApiCall'));
+    }
+
+    if (sfConfig::get('sf_web_debug', false))
+    {
+      $this->dispatcher->connect('debug.web.load_panels', array('sfWebDebugPanelFacebook', 'listenToLoadDebugWebPanelEvent'));
+    }
   }
 }
