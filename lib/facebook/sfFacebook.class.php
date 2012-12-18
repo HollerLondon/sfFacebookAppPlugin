9d499a0284dfae3f82ba4ba9080f15513e5e11f3
Jo Carter <github@jocarter.co.uk>
2012-12-18 12:27:01 -0800

    Adding in some new functionality for improve cacophony

--- lib/facebook/sfFacebook.class.php
+++ lib/facebook/sfFacebook.class.php
@@ -90,4 +90,73 @@ class sfFacebook extends Facebook
     self::$instance = self::$instance ? self::$instance : new sfFacebook($options);
     return self::$instance;
   }
+  
+  /**
+   * Override to get from sfUser session
+   * 
+   * @see Facebook::getPersistentData()
+   */
+  protected function getPersistentData($key, $default = false) 
+  {
+    $value = parent::getPersistentData($key, $default);
+    
+    try {
+      return sfContext::getInstance()->getUser()->getAttribute($key, $value);
+    }
+    catch (Exception $e) {
+      return $value;
+    }
+  }
+  
+  /**
+   * Retrieves an access token for the given authorization code
+   * (previously generated from www.facebook.com on behalf of
+   * a specific user).  The authorization code is sent to graph.facebook.com
+   * and a legitimate access token is generated provided the access token
+   * and the user for which it was generated all match, and the user is
+   * either logged in to Facebook or has granted an offline access permission.
+   *
+   * Get params including expiry for cacophony stuffs - includes expires
+   *
+   * @param string $code An authorization code.
+   * @return array An access token exchanged for the authorization code, or
+   *               false if an access token could not be generated.
+   */
+  public function getAccessTokenParamsFromCode($code, $redirect_uri = null) {
+    if (empty($code)) {
+      return false;
+    }
+
+    if ($redirect_uri === null) {
+      $redirect_uri = $this->getCurrentUrl();
+    }
+
+    try {
+      // need to circumvent json_decode by calling _oauthRequest
+      // directly, since response isn't JSON format.
+      $access_token_response =
+        $this->_oauthRequest(
+          $this->getUrl('graph', '/oauth/access_token'),
+          $params = array('client_id' => $this->getAppId(),
+                          'client_secret' => $this->getAppSecret(),
+                          'redirect_uri' => $redirect_uri,
+                          'code' => $code));
+    } catch (FacebookApiException $e) {
+      // most likely that user very recently revoked authorization.
+      // In any event, we don't have an access token, so say so.
+      return false;
+    }
+
+    if (empty($access_token_response)) {
+      return false;
+    }
+
+    $response_params = array();
+    parse_str($access_token_response, $response_params);
+    if (!isset($response_params['access_token'])) {
+      return false;
+    }
+
+    return $response_params;
+  }
 } // END
