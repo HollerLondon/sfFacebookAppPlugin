2bcd9985a57de7b184abb246d90c81e4765a9fd2
Jo Carter <github@jocarter.co.uk>
2012-12-18 12:38:34 -0800

    Adding in some new functionality for improve facebook access token bits

--- lib/facebook/sfFacebook.class.php
+++ lib/facebook/sfFacebook.class.php
@@ -159,4 +159,52 @@ class sfFacebook extends Facebook
 
     return $response_params;
   }
+  
+  /**
+   * Extend an access token, while removing the short-lived token that might
+   * have been generated via client-side flow. Thanks to http://bit.ly/b0Pt0H
+   * for the workaround.
+   * 
+   * @return array of params
+   * @throws FacebookApiException
+   */
+  public function getExtendedAccessToken() {
+    try {
+      // need to circumvent json_decode by calling _oauthRequest
+      // directly, since response isn't JSON format.
+      $access_token_response = $this->_oauthRequest(
+        $this->getUrl('graph', '/oauth/access_token'),
+        $params = array(
+          'client_id' => $this->getAppId(),
+          'client_secret' => $this->getAppSecret(),
+          'grant_type' => 'fb_exchange_token',
+          'fb_exchange_token' => $this->getAccessToken(),
+        )
+      );
+    }
+    catch (FacebookApiException $e) {
+      // most likely that user very recently revoked authorization.
+      // In any event, we don't have an access token, so say so.
+      throw $e;
+    }
+
+    if (empty($access_token_response)) {
+      return false;
+    }
+
+    $response_params = array();
+    parse_str($access_token_response, $response_params);
+
+    if (!isset($response_params['access_token'])) {
+      return false;
+    }
+
+    $this->destroySession();
+
+    $this->setPersistentData(
+      'access_token', $response_params['access_token']
+    );
+    
+    return $response_params;
+  }
 } // END
